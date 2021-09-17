11a84241613288931c5a594183d834ba56fbb097
Eadjei25 <asa20005@byui.edu>
2021-09-17 06:53:08 +0000

    week 1 Final Commit

--- week01/example/script.js
+++ week01/example/script.js
@@ -1,20 +1,23 @@
-//Read and retrieve the story name stored in the local storage
-function loadStory(){
-    let storyName= document.getElementById('name').value;
-    let storyHTML= localStorage.getItem(storyName);
-    document.getElementById('story').value= storyHTML;
-}
 //Save the story name as a key to access the story in the browser 
 function saveStory(){
     let storyName= document.getElementById('name').value;
     let storyHTML= document.getElementById('story').value;
     localStorage.setItem(storyName, storyHTML);
 }
+
+//Read and retrieve the story name stored in the local storage
+function loadStory(){
+    let storyName= document.getElementById('name').value;
+    let storyHTML= localStorage.getItem(storyName);
+    document.getElementById('story').value= storyHTML;
+}
+
 //Simply displays the story in a div below the story input field
 function displayStory(){
     let storyHTML= document.getElementById('story').value;
     document.getElementById('display').innerHTML= storyHTML;
 }	
+
 //Delete the data
 function deleteStory(){
     let storyName= document.getElementById('name').value;
@@ -28,3 +31,5 @@ function deleteStory(){
         window.alert('Incorrectname or no input');
     }
 }
+
+//To avoid declaring the same vars, you can declare the var outside of a block and access it inside the blocks you need it.
