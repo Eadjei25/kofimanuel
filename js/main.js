e5a1471895a9548884f413b922b514e81da5e1de
Eadjei25 <asa20005@byui.edu>
2022-04-09 14:54:20 +0000

    final project

--- js/main.js
+++ js/main.js
@@ -1,77 +1,41 @@
-//Table of Content 
-const links= [
-    {
-        label: 'Week01 Notes and Exercises',
-        url: 'week01/',
-        title: 'Week 1 link'
-    },
-    {
-        label: 'Week02 Notes and Exercises',
-        url: 'week02/',
-        title: 'Week 2 Link'
-    },
-    {
-        label: 'Week03 Notes and Exercises',
-        url: 'week03/',
-        title: 'Week 3 Link'
-    },
-    {
-        label: 'Week04 Notes and Exercises',
-        url: 'week04/',
-        title: 'Week 4 Link'
-    },
-    {
-        label: 'Week05 Notes and Exercises',
-        url: 'week05/',
-        title: 'Week 5 Link'
-    },
-    {
-        label: 'Week06 Challenge and Notes',
-        url: 'week06/',
-        title: 'Week 6 Link'
-    },
-    {
-        label: 'Week07 Notes and Exercises',
-        url: 'week07/',
-        title: 'Week 7 Link'
-    },
-    {
-        label: 'Week08 Notes an Exercises',
-        url: 'week08/',
-        title: 'Week 8 Link',
-    },
-    {
-        label: 'Week09 Notes and Exercises',
-        url: 'week09/',
-        title: 'Week 9 Link'
-
-    },
-    {
-        label: 'Week10 Notes and Exercises',
-        url: 'week10/',
-        title: 'Week 10 Link'
-    },
-    {
-        label: 'Week11 Notes and Exercises',
-        url: 'week11/',
-        title: 'Week 11 Link'
-    },
-    {
-        label: 'Week12 Notes and Exercises',
-        url: 'week12/',
-        title: 'Week 12 Link'
-    },
-    {
-        label: 'Week13 Notes and Exercises',
-        url: 'week13/',
-        title: 'Week 13 Link'
-    },
-    {
-        label: 'Week14 Notes and Exercises',
-        url: 'week14/',
-        title: 'Week 14 Link'
-    }
-]
+//Table of Content
+const links = [
+	{
+		label: "Week01 Notes and Exercises",
+		url: "week01/",
+		title: "Week 1 link",
+	},
+	{
+		label: "Week02 Notes and Exercises",
+		url: "week02/",
+		title: "Week 2 Link",
+	},
+	{
+		label: "Week03 Notes and Exercises",
+		url: "week03/",
+		title: "Week 3 Link",
+	},
+	{
+		label: "Week04 Notes and Exercises",
+		url: "week04/",
+		title: "Week 4 Link",
+	},
+	{
+		label: "Week05 Notes and Exercises",
+		url: "week05/",
+		title: "Week 5 Link",
+	},
+	{
+		label: "Week06 Challenge and Notes",
+		url: "week06/",
+		title: "Week 6 Link",
+	},
+	{
+		label: "Week07 Notes and Exercises",
+		url: "week07/",
+		title: "Week 7 Link",
+	},
+];
 /*
 function contentTable(){
     for(let i=0; i < links.length; i++){
@@ -88,13 +52,13 @@ function contentTable(){
 contentTable();
 */
 
-const elem= document.getElementsByTagName('ol');
-links.forEach(link => {
-    const list= document.createElement('li');
-    const anchor= document.createElement('a');
-    anchor.setAttribute('href', link.url);
-    anchor.setAttribute('title', link.title);
-    anchor.innerText= link.label;
-    list.appendChild(anchor);
-    elem[0].appendChild(list);
-})
+const elem = document.getElementsByTagName("ol");
+links.forEach((link) => {
+	const list = document.createElement("li");
+	const anchor = document.createElement("a");
+	anchor.setAttribute("href", link.url);
+	anchor.setAttribute("title", link.title);
+	anchor.innerText = link.label;
+	list.appendChild(anchor);
+	elem[0].appendChild(list);
+});
---
+++ project/js/utils.js
@@ -0,0 +1,92 @@
+export const APIKey = "5c78240201b345f0bb87ad182cdeda5e";
+export const recipesDiv = document.getElementById("recipes-div");
+
+export async function search(string) {
+	try {
+		let response = await fetch(
+			`https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKey}&query=${string}&addRecipeInformation=true&addRecipeNutrition=true`
+		);
+		if (!response.ok) {
+			let error = document.createElement("h3");
+			error.classList.add("error");
+			if (response.status == 402) {
+				error.textContent =
+					"Sorry, all possible searches have been made today. Try Again tomorrow!";
+				recipesDiv.appendChild(error);
+			} else {
+				error.textContent = "Oops, something went wrong. Please try again.";
+				recipesDiv.appendChild(error);
+			}
+		}
+		let data = await response.json();
+		return data;
+	} catch (e) {
+		window.alert(`Oops, there was an issue: ${e}`);
+	}
+}
+
+export async function createMealCard(meals) {
+	meals.forEach((meal) => {
+		let div = document.createElement("div");
+		let h3 = document.createElement("h3");
+		let img = document.createElement("img");
+		let recipe = document.createElement("p");
+		let recipeLink = document.createElement("a");
+
+		div.classList.add("card");
+		recipeLink.classList.add("recipe-link");
+
+		recipeLink.setAttribute("href", meal.sourceUrl);
+		img.setAttribute("alt", `Image of ${meal.title}`);
+		img.setAttribute("width", "400");
+		img.setAttribute("src", meal.image);
+
+		h3.innerHTML = meal.title;
+		recipeLink.innerHTML = meal.sourceUrl;
+
+		recipe.appendChild(recipeLink);
+
+		div.appendChild(h3);
+		div.appendChild(img);
+		div.appendChild(recipe);
+
+		recipesDiv.appendChild(div);
+	});
+}
+export async function createFooterYear() {
+	let d = new Date();
+	let yyyy = d.getFullYear();
+	//find span
+	let yearSpan = document.getElementById("year");
+	//place current year in span
+	yearSpan.innerHTML = yyyy;
+}
+export async function loadHeaderFooter() {
+	const header = await loadTemplate("./partials/header/main-header.html");
+	const footer = await loadTemplate("./partials/footer/main-footer.html");
+	const headerElement = document.getElementById("main-header");
+	const footerElement = document.getElementById("main-footer");
+	renderWithTemplate(header, headerElement);
+	renderWithTemplate(footer, footerElement);
+	createFooterYear();
+}
+export async function loadTemplate(path) {
+	const html = await fetch(path).then(convertToText);
+	const template = document.createElement("template");
+	template.innerHTML = html;
+	return template;
+}
+function convertToText(res) {
+	if (res.ok) {
+		return res.text();
+	} else {
+		throw new Error("Bad Response");
+	}
+}
+export async function renderWithTemplate(template, parent, data, callback) {
+	let clone = template.content.cloneNode(true);
+	if (callback) {
+		clone = callback(clone, data);
+	}
+	parent.appendChild(clone);
+}
